65ab51c94398aae44ca7974cba2cbe2d8f0d929d
boraozer <nagayapim@gmail.com>
2022-02-12 03:42:14 +0300

    disconnect listener refactoring done

--- src/events/disconnect.js
+++ src/events/disconnect.js
@@ -3,15 +3,6 @@ const name = 'disconnect';
 async function listener(socket, network)
 {
     const id = socket.id;
-
-    const connectionList = network.getClientConnections(id);
-    if(connectionList.length > 0) {
-        connectionList.forEach(offerId => {
-            network.dropClientConnection(offerId, id);
-        });
-    }
-
-    socket.broadcast.emit('drop', id);
     network.disconnectClient(id);
 }
 
