ca237b112c0f96f7e4c09953a99c07a550a807db
boraozer <nagayapim@gmail.com>
2022-08-17 03:14:48 +0300

    update demo bridge node url

--- develop/app.js
+++ develop/app.js
@@ -3,7 +3,7 @@ const foxql = require('../index.js');
 
 const port = process.argv[2] || 1924
 const server = new foxql.server(connection, [
-    'http://127.0.0.1:1923' // bridge server url
+    'https://foxql-bridge.herokuapp.com' // bridge server url
 ]);
 
 server.use('serverOptions', {
